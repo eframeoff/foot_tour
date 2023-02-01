372899d07d65a5853743e6e1df86c6b4d60185a7
eframeoff <vacra@mail.ru>
2023-02-01 21:29:02 +0300

    feat: установлен и настроен webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,19 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
+  stats: { warnings: false },
   entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
   },
   module: {
     rules: [
